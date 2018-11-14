10829cb4afd0ce9243e073ca3fe83db59c8dcc55
ppeters0502 <pspeters93@gmail.com>
2018-11-14 10:20:45 -0600

    We now have a working Client!!!
    Still need to test over the internet, and connect users in easyrtc to
    okta, but we have initial contact!!

--- easyrtc/server_example/static/js/script.js
+++ easyrtc/server_example/static/js/script.js
@@ -24,16 +24,35 @@ if(!location.hash){
 const chatHash = location.hash.substring(1);
 
 function addToConversation(who, msgType, content){
+  debugger;
   //Need to escape any special characters
   content = content.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;');
   content = content.replace(/\n/g, '<br />');
-  const template = document.querySelector('template[data-template="message"]');
-  const nameEl = template.content.querySelector('.message__name');
-  nameEl.innerText = who;
-  template.content.querySelector('.message__bubble').innerText = content;
-  const clone = document.importNode(template.content, true);
-  const messageEl = clone.querySelector('.message');
-  messageEl.classList.add('message--theirs');
+  // const template = document.querySelector('template[data-template="message"]');
+  // const nameEl = template.content.querySelector('.message__name');
+  // nameEl.innerText = who;
+  // template.content.querySelector('.message__bubble').innerText = content;
+  // const clone = document.importNode(template.content, true);
+  // const messageEl = clone.querySelector('.message');
+  // messageEl.classList.add('message--theirs');
+  // If message is from me, then style appropriately
+  var messageWindow = document.getElementById("messageWindow");
+  if(who=="Me")
+  {
+    messageWindow.innerHTML += "<div class='message message--mine'><span class='message__name'>"+who+"</span><div class='message__bubble'><p>"+content+"</p></div></div>";
+  }
+  else
+  {
+    messageWindow.innerHTML += "<div class='message message--theirs'><span class='message__name'>"+who+"</span><div class='message__bubble'><p>"+content+"</p></div></div>";
+  }
+  
+
+  // <div class="message message--mine">
+  //       <div class="message__bubble"><p>Hello?</p></div>
+  //     </div>
+  //     <div class="message message--theirs">
+  //       <div class="message__bubble"><p>nope!</p></div>
+  //     </div>
 }
 
 function connect(){
@@ -44,9 +63,7 @@ function connect(){
     var userSelector = document.getElementById("userSelect");
     var easyrtcid = userSelector.options[userSelector.selectedIndex].value;
             console.log("easyrtcid: "+easyrtcid);
-              return function() {
-                  sendStuffWS(easyrtcid);
-              }
+              return sendStuffWS(easyrtcid);        
   });
   easyrtc.connect("easyrtc.instantMessaging", loginSuccess, loginFailure);
 
@@ -61,6 +78,7 @@ function convertListToIcons(roomName, occupants, isPrimary){
 }
 
 function sendStuffWS(otherEasyrtcid) {
+   // debugger;
     var text = document.getElementById('sendMessageText').value;
     console.log("text from sendStuffWS: "+text);
     if (text.replace(/\s/g, "").length === 0) { // Don't send just whitespace
